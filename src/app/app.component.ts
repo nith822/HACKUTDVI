b093c600319c2990b18f8e913b4273be747e89eb
houkagoteatime <1harveygao@gmail.com>
2019-11-09 22:55:17 -0600

    add basic map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,15 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  Input,
+  Output,
+  EventEmitter, 
+  OnDestroy
+} from "@angular/core";
+import { loadModules } from "esri-loader";
+import esri = __esri; // Esri TypeScript Types
 
 @Component({
   selector: 'app-root',
@@ -6,5 +17,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'truck2';
+
+  // The <div> where we will place the map
+  @ViewChild("mapViewNode", { static: true }) private mapViewEl: ElementRef;
+  private _view: esri.MapView = null;
+  private _basemap = "streets";
+  private _center: Array<number> = [0.1278, 51.5074];
+  private _zoom = 10;
+
+  constructor() {}
+
+  async initializeMap() {
+    try {
+      // Load the modules for the ArcGIS API for JavaScript
+      const [EsriMap, EsriMapView] = await loadModules([
+        "esri/Map",
+        "esri/views/MapView"
+      ]);
+
+      // Configure the Map
+      const mapProperties: esri.MapProperties = {
+        basemap: this._basemap
+      };
+
+      const map: esri.Map = new EsriMap(mapProperties);
+
+      // Initialize the MapView
+      const mapViewProperties: esri.MapViewProperties = {
+        container: this.mapViewEl.nativeElement,
+        center: this._center,
+        zoom: this._zoom,
+        map: map
+      };
+
+      this._view = new EsriMapView(mapViewProperties);
+      await this._view.when();
+      return this._view;
+    } catch (error) {
+      console.log("EsriLoader: ", error);
+    }
+  }
+
+  ngOnInit() {
+    // Initialize MapView and return an instance of MapView
+    this.initializeMap().then(mapView => {
+      // The map has been initialized
+      console.log("mapView ready: ", this._view.ready);
+    });
+  }
+
+  ngOnDestroy() {
+    if (this._view) {
+      // destroy the map view
+      this._view.container = null;
+    }
+  }
 }
